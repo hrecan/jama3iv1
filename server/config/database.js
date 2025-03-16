a436c704328c16bc851f0d514e5a86a35b454b21
hrecan <129568092+hrecan@users.noreply.github.com>
2025-03-16 23:41:32 +0100

    Configure for Railway deployment

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Asmarh06072024*',
-    database: 'jama3iv001',
-    port: 3306
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'Asmarh06072024*',
+    database: process.env.DB_NAME || 'jama3iv001',
+    port: process.env.DB_PORT || 3306
 };
